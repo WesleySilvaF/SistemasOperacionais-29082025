5cf2ee926fc15893ac20e89b97d183f1dde82af9
Wesley <wesleydeler@gmail.com>
2025-09-10 21:29:57 -0300

    Add files via upload

--- arvthread.py
+++ arvthread.py
@@ -1,16 +1,15 @@
 #importação de biblioteca
-import threading
-import time
-import math
+import threading #Importa Demódulos
+import time #Importa Demódulos
+import math #Importa Demódulos
 #Estrutura da Thread
 def estruturaThread(nome, incio, fim):
     for i in range(incio, fim +1):
         print(f'{nome} -> {i}')
-        time.sleep(0)
+        time.sleep(1) #Tempo de Resposta para cada contagem (EX: time.sleep(2) usado para definir 2 segundos de tempo de resposta)
 
-thread1 = threading.Thread(target=estruturaThread, args=("thread1", 1, 10))
-thread2 = threading.Thread(target=estruturaThread, args=("thread2", 11, 20))
+thread1 = threading.Thread(target=estruturaThread, args=("thread1", 1, 10)) #É a função usada definir a estrutura de contagem
+thread2 = threading.Thread(target=estruturaThread, args=("thread2", 50, 60)) #É a função usada definir a estrutura de contagem
 
-thread1.start()
-thread2.start()
-
+thread1.start() #Comando usado para iniciar os funções
+thread2.start() #Comando usado para iniciar as funções
